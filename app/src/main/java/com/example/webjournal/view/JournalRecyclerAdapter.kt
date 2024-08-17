7fa4175fc2fd5ae575d4f55d983545913c562366
Andant3 <kamikadze3571@gmail.com>
2024-08-17 22:10:31 +0200

    Implementing RecyclerViewAdapter functionality

--- app/src/main/java/com/example/webjournal/view/JournalRecyclerAdapter.kt
+++ app/src/main/java/com/example/webjournal/view/JournalRecyclerAdapter.kt
@@ -4,34 +4,39 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
-import com.example.webjournal.R
 import com.example.webjournal.databinding.PageItemBinding
 import com.example.webjournal.model.Journal
 
-class JournalRecyclerAdapter(val context: Context, var journalList: List<Journal>): RecyclerView.Adapter<JournalRecyclerAdapter.JournalViewHolder>() {
+class JournalRecyclerAdapter(private val context: Context, private var journalList: List<Journal>): RecyclerView.Adapter<JournalRecyclerAdapter.JournalViewHolder>() {
 
 
     private lateinit var binding: PageItemBinding
 
-    class JournalViewHolder(itemView: View, context: Context): ViewHolder(itemView){
+    class JournalViewHolder(var binding: PageItemBinding)
+        : ViewHolder(binding.root){
 
+            fun bind(journal: Journal){
+                binding.journal = journal
+            }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): JournalViewHolder {
-        val view = LayoutInflater.from(context).inflate(R.layout.page_item, parent, false)
-        return JournalViewHolder(view, context)
+        //val view = LayoutInflater.from(context).inflate(R.layout.page_item, parent, false)
+
+        binding = PageItemBinding.inflate(
+            LayoutInflater.from(parent.context),
+            parent,
+            false
+        )
+        return JournalViewHolder(binding)
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount(): Int = journalList.size
 
     override fun onBindViewHolder(holder: JournalViewHolder, position: Int) {
-
         val journal: Journal = journalList.get(position)
-        var imageURL: String = ""
+        holder.bind(journal)
     }
 }
